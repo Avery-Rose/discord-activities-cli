2d9ca07529e1486374a1bb57b316632cee719a43
Avery <avery.doucet03@gmail.com>
2022-08-16 20:08:32 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const readLine = require('readline');
+const AsciiTable = require('ascii-table');
+const req = require('./request');
+const apps = require('./apps.json');
+
+require('colors');
+require('dotenv').config();
+
+const exit = () => {
+  console.log('\nExiting...');
+  rl.close();
+  process.exit();
+};
+
+const copy = (text) => {
+  require('child_process').spawn('clip').stdin.end(text);
+  console.log(`Copied to clipboard: `.green + text.blue);
+};
+
+var token = '';
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('DISCORD TOKEN: ', async (answer) => {
+  if (answer) {
+    token = answer;
+    process.stdout.write('\x1Bc'); // clear the screen
+    printActivityTable();
+    question();
+  } else {
+    process.stdout.write('\x1Bc');
+    console.log('No token provided.'.red);
+    exit();
+  }
+});
+
+const printActivityTable = () => {
+  let table = new AsciiTable();
+
+  table.setHeading('Activity', 'ID');
+  for (let key in apps) {
+    table.addRow(key, apps[key]);
+  }
+  console.log(table.toString().red);
+};
+
+const question = async () => {
+  console.log(
+    'What channel do you want to create the activity in?:\n'.underline.magenta +
+      '(Channel ID) (Activity Type)'.blue
+  );
+  rl.question('', async (answer) => {
+    const args = answer.split(' ');
+
+    if (!args[0]) {
+      return question();
+    }
+
+    if (!args[1]) {
+      args[1] = 'youtube';
+    }
+
+    if (answer == 'exit') {
+      return exit();
+    }
+
+    const invite = await req(token, args[0], args[1]);
+    if (!invite) return exit();
+    copy(`<${invite}>`);
+    question();
+  });
+};
---
+++ request.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch');
+const apps = require('./apps.json');
+
+/**
+ * @param {string} token
+ * @param {string} id the id of the target channel
+ * @param {string} activity the id of the activity
+ * @returns {string} the invite link
+ */
+async function req(token, id, activity) {
+  const res = await fetch(
+    `https://discord.com/api/v10/channels/${id}/invites`,
+    {
+      method: 'POST',
+      headers: {
+        authorization: token,
+        'content-type': 'application/json',
+      },
+
+      body: JSON.stringify({
+        max_age: 86400,
+        max_uses: 0,
+        temporary: false,
+        unique: true,
+        target_type: 2,
+        target_application_id: apps[activity],
+        validate: null,
+      }),
+    }
+  );
+  if (res.status === 200) {
+    const invite = await res.json();
+    return `https://discord.com/invite/${invite.code}`;
+  } else {
+    process.stdout.write('\x1Bc');
+    console.error(
+      new Error(`Discord API: ${res.status}. Invalid ChannelID?`.red)
+    );
+    return '';
+  }
+}
+
+module.exports = req;
